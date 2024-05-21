919084d9d53561fe94318e00c17653dbb5c4c4cf
David Norton <pie_masters@hotmail.co.uk>
2024-05-21 16:44:27 +0100

    feat: add css module support

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,15 @@ import { fileURLToPath } from "node:url";
 import { glob } from "glob";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"], exclude: ["**/*.stories.*"] })],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ include: ["lib"], exclude: ["**/*.stories.*"] }),
+  ],
   build: {
     copyPublicDir: false,
     lib: {
