65b4117c991c2f917cd827c0236122fed62dcd8c
David Norton <pie_masters@hotmail.co.uk>
2024-05-21 16:14:40 +0100

    feat: add storybook

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
-import { resolve } from "path";
+import { extname, relative, resolve } from "path";
+import { fileURLToPath } from "node:url";
+import { glob } from "glob";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "tailwindcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ include: ["lib"] })],
+  plugins: [react(), dts({ include: ["lib"], exclude: ["**/*.stories.*"] })],
   build: {
     copyPublicDir: false,
     lib: {
@@ -15,6 +17,22 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime", "tailwindcss"],
+      input: Object.fromEntries(
+        glob
+          .sync("lib/**/*.{ts,tsx}", {
+            ignore: ["lib/**/*.d.ts", "lib/**/*.stories.*"],
+          })
+          .map((file) => [
+            // The name of the entry point - lib/nested/foo.ts becomes nested/foo
+            relative("lib", file.slice(0, file.length - extname(file).length)),
+            // The absolute path to the entry file - lib/nested/foo.ts becomes /project/lib/nested/foo.ts
+            fileURLToPath(new URL(file, import.meta.url)),
+          ])
+      ),
+      output: {
+        assetFileNames: "assets/[name][extname]",
+        entryFileNames: "[name].js",
+      },
     },
   },
   css: {
